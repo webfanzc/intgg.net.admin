1801c4cb571eb7466b72e17a7323a178ac929c5c
zhuhonglei <tvmining>
2017-06-04 12:18:14 +0800

    init

--- models/index.js
+++ models/index.js
@@ -15,31 +15,15 @@ var db = mongoose.createConnection(config.mongodb.str, config.mongodb.opts, func
 });
 
 
-require('./users');
-require('./usersauth');
 require('./materials');
 require('./setups');
-require('./interactions');
-require('./droppacks');
-require('./dropcenter');    //投放中心
-require('./ondemands');    //投放中心
-require('./crazygrab');    //疯抢
 
 require('./user');
-require('./message');
 
-exports.Users = db.model('Users');
-exports.UsersAuth = db.model('UsersAuth');
 exports.Materials = db.model('Materials');
 exports.Setups = db.model('Setups');
-exports.Interactions = db.model('Interactions');
-exports.Droppacks = db.model('Droppacks');
-exports.Dropcenter = db.model('Dropcenter');
-exports.Ondemands = db.model('Ondemands');
-exports.Crazygrab = db.model('Crazygrab');
 
 exports.User = db.model('User');
-exports.Message = db.model('Message');
 
 exports.close = function(){
     db.close(function(){
