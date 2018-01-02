a003809f69061b7cb088004f5212dc617a9ccf80
webfanzc <webfanzc@126.com>
2018-01-02 14:49:18 +0800

    订单

--- assets/app/admin_index/setups_verified/setups_verifide.service.ts
+++ assets/app/admin_index/setups_verified/setups_verifide.service.ts
@@ -2,11 +2,11 @@
  * Created by gy104 on 17/5/13.
  */
 import {Injectable} from "@angular/core";
-import {Http,Headers} from "@angular/http"
+import {Http, Headers} from "@angular/http"
 /**
  * Created by gy104 on 17/3/23.
  */
-import { Setups } from './setups.model'
+import {Setups} from './setups.model'
 import {Subject} from "rxjs";
 import {Router} from "@angular/router";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
@@ -15,28 +15,30 @@ import {SnackBarService} from "../../share/toast/snackbar.service";
 @Injectable()
 export class SetupsVerifiedService {
     private setups: Setups[] = [];
-    public  setupsSuject: Subject<Setups[]> = new Subject<Setups[]>();
+    public setupsSuject: Subject<Setups[]> = new Subject<Setups[]>();
     public setupIndex: Subject<number> = new Subject<number>();
     private path = "?token=" + localStorage.getItem('token');
     private headers = {
-        headers: new HttpHeaders().set('Content-Type','application/json')
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
     };
-    constructor(private httpService: HttpClient,private router: Router,private snackbar: SnackBarService){}
+
+    constructor(private httpService: HttpClient, private router: Router, private snackbar: SnackBarService) {
+    }
 
 
-
-    updateSetup(index: number,setups: Setups){
+    updateSetup(index: number, setups: Setups) {
         let _id = setups._id;
         let body = JSON.stringify(setups);
-        const prepath =  "/setups/save" + this.path+'&_id=' + _id;;
+        const prepath = "/setups/save" + this.path + '&_id=' + _id;
+        ;
 
-        this.httpService.post<any>(prepath,body, this.headers)
+        this.httpService.post<any>(prepath, body, this.headers)
             .toPromise()
             .then(
                 (response) => {
                     let res = response;
                     let data = res.data;
-                    if(res.status == 200) {
+                    if (res.status == 200) {
                         this.setups[index] = new Setups(
                             data.brand,
                             data.brandlogo,
@@ -53,7 +55,7 @@ export class SetupsVerifiedService {
                             data._id
                         )
                     }
-                    if(res.status != 200){
+                    if (res.status != 200) {
                         console.log(res);
                         // this.snackBar.open(res.msg,'', {
                         //     duration: 3000
@@ -66,32 +68,33 @@ export class SetupsVerifiedService {
 
     }
 
-    getSetup(index: number){
+    getSetup(index: number) {
         return this.setups[index];
     }
 
 
     goNet(intid: string) {
-       // window.location.href = 'http://localhost:3001/dashboard/admin?intid='+intid;
+        // window.location.href = 'http://localhost:3001/dashboard/admin?intid='+intid;
 
-        let path = 'http://dev.intgg.net/dashboard/admin?intid='+intid;
+        let path = 'http://dev.intgg.net/dashboard/admin?intid=' + intid;
         window.open(path, '_blank');
 
         // this.httpService.get(path)
         //     .toPromise()
         //     .then()
     }
-    getSetups(): Promise<Setups>{
-        const repath = '/setups/list'+this.path;
+
+    getSetups(): Promise<Setups> {
+        const repath = '/setups/list' + this.path;
 
         return this.httpService.get<any>(repath)
             .toPromise()
             .then(
                 (response) => {
                     let res = response;
-                    let setups : Setups[] = [];
-                    if(res.status == 200){
-                        for(let data of res.data) {
+                    let setups: Setups[] = [];
+                    if (res.status == 200) {
+                        for (let data of res.data) {
                             setups.push(
                                 new Setups(
                                     data.brand,
